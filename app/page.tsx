2048e72600fbb88a599989d5d24ad61f18a07b42
FalconXD <gustavo.sousa12@gmail.com>
2025-06-04 08:51:09 -0300

    Criação da página home e seus componentes (Companions e CTA)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,51 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import Cta from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SASS App</h1>
-      <Button>
-        Let`s get start
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl'>Popular Companions</h1>
+      
+      <section className='home-section'>
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard 
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature "
+          subject="Language"
+          duration={30}
+          color="#8DE7FF"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList 
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
   )
 }
 
