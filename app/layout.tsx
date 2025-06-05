cc80c0310583aace91d53eefa89a84762b223b88
FalconXD <gustavo.sousa12@gmail.com>
2025-06-05 07:43:36 -0300

    Integação com o clerk. Utilização de autenticação, seleção de planos e pagamento

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -18,12 +19,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+  return (    
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+          <ClerkProvider appearance={{variables: { colorPrimary: '#fe5933'}}}>
+            <Navbar />
+            {children}
+          </ClerkProvider>
+        </body>
+      </html>
   );
 }
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,12 @@
+import { PricingTable } from "@clerk/nextjs"
+
 const Subscrition = () => {
   return (
-    <div>Subscrition</div>
+    <div>
+      <main>
+        <PricingTable/>
+      </main>
+    </div>
   )
 }
 
